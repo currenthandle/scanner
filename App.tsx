50a09460e27bf1f6c3d7f0baec255ff69b6dcb5b
Casey <leptone55@gmail.com>
2023-09-08 17:40:08 -0700

    tailwind hacky types

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/// <reference types="nativewind/types" />
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+import Scanner from './components/Scanner'
+// import React from 'react'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text>Open up App.tsx to start working on your app!!!</Text>
+      <StatusBar style='auto' />
+      <Text>Open up App.tsx to start working on your app!!!</Text>
+      <Scanner />
+      {/* <span>hello</span> */}
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     paddingTop: 200,
+//     height: 100000,
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// })
